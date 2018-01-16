202290b3839da4f30505b9fbd76d0003987179c1
Ilma Kosa <ikosa@brainsum.com>
2018-01-16 12:19:18 +0200

    TER-61 | modifications in URLs

--- configs/site.config.js
+++ configs/site.config.js
@@ -1,6 +1,7 @@
 var baseURL = 'http://localhost:8000/'; // http://www.eterracotta.hu/';
 
 exports.siteURL = baseURL;
-exports.flowerStandsURL = baseURL + 'kategoria/viragtartok/';
-exports.clayPotsURL = baseURL + 'agyagcserep/';
-exports.cartURL = baseURL + 'kosar/';
+exports.loginURL = baseURL + 'user/login';
+exports.flowerStandsURL = baseURL + 'viragtartok';
+exports.clayPotURL = baseURL + 'viragtartok/agyag-viragtartok/agyagcserep';
+exports.cartURL = baseURL + 'kosar';
